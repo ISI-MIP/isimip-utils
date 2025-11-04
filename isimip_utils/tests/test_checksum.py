11e5c5380ec5d1236bbbdb0e19b8b50d9167831d
Jochen Klar <mail@jochenklar.de>
2025-11-04 16:49:56 +0100

    Add tests

---
+++ isimip_utils/tests/test_checksum.py
@@ -0,0 +1,17 @@
+from pathlib import Path
+
+from isimip_utils.checksum import get_checksum, get_checksum_suffix, get_checksum_type
+
+
+def test_get_checksum():
+    file_path = Path('testing/datasets') / 'ISIMIP3a/InputData/geo_conditions/landseamask/landseamask.nc'
+    checksum = get_checksum(file_path)
+    assert checksum == '30f34d0720b8a6b670d0c093d488a3cd564e232a94d7ebafef99c1d7c18cec5d127fbc663f6378b4b99f9434fa10f71e8413b533c5cc5314d149ab9e2f7cca98'  # noqa: E501
+
+
+def test_get_checksum_type():
+    assert get_checksum_type() == 'sha512'
+
+
+def test_get_checksum_suffix():
+    assert get_checksum_suffix() == '.sha512'
